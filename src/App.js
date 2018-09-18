bea6517b19145373684b61b3f6f8e76d19101b2d
Raul Sanchez <raulsg93@gmail.com>
2018-09-18 14:03:32 -0500

    working app, takes score and shuffles the guitars

--- src/App.js
+++ src/App.js
@@ -6,61 +6,81 @@ import './App.css';
 import _ from './clicky.json'
 
 class App extends Component {
-  state ={
+  state = {
     _, //include array of guitars
-    newScore : 0, //highScore for display purposes
-    currentScore : 0, //incrementing changing score during click
-    
+    newScore: 0, //highScore for display purposes
+    currentScore: 0, //incrementing changing score during click\
+    clicked: [], //an array to add to clicked
+
   }
-      shuffle = (_)=>{
-       _.sort(function (a, b) { return 0.5 - Math.random() });
-       return _;
-     }
+  shuffle = (_) => {
+    _.sort(function (a, b) { return 0.5 - Math.random() });
+    return _;
+  }
 
   //handleClick for truth/false comparsion
   //check if component is true or false this.state."handlclick" <<whatever function call truth
-   handleScore = () =>{
-    if(!this.state){
-      const newScore =this.state.newScore+1;
-
-      this.setState({newScore: newScore})
-      this.setState({currentScore: newScore})
-    }
-    else{
-      alert('oh snap you failed!')
-
-    }
-  }
-
-  //resetgame if 
-  resetGame = () =>{
+  handleIncrement = () =>{
+    const score = this.state.currentScore+1;
     this.setState({
-      _, //include array of guitars
-      newScore : 0, //highScore for display purposes
-      currentScore : 0,
+      currentScore: score
     })
-    this.shuffle(this.state._);
+    if(this.state.currentScore > this.state.newScore){
+      const newHighScore = this.state.currentScore+1;
+      this.setState({newScore: newHighScore})
+    }
+   
   }
 
-  //and handle increment for scores
-  render() {
-    return (
-      <div className = 'container'>
-      <Navbar 
-      currentScore={this.state.currentScore}
+  handleClick = (id) => {
+    console.log("clicked")
+    console.log("id is " + id);
+    //id is passing and click is logging, we can change state here and reposition
+    if (this.state.clicked.indexOf(id) === -1) { //check array of already clicked clickys and IF NOT THERE
+      this.handleIncrement();                 //some other function to handle points
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      alert("oh no!");
+      this.resetGame();
+    }
+  };
+
+
+
+
+//resetgame if 
+resetGame = () => {
+  this.setState({
+    _, //include array of guitars
+    currentScore: 0,
+  })
+  this.shuffle(this.state._);
+}
+
+//and handle increment for scores
+render() {
+  return (
+    <div className='container'>
+      <Navbar
+        currentScore={this.state.currentScore}
         highScore={this.state.newScore}
       />
-      <Jumbotron/>
-      
-        
-      {this.shuffle(this.state._).map(image =>(
-        <Clicky image= {image.image} />
+      <Jumbotron />
+
+
+      {this.shuffle(this.state._).map(image => (
+        <Clicky image={image.image}
+          id={image.id}
+
+          click={this.handleClick}
+
+        />
       ))}
-     
-      </div>
-       
-    );
-  }
+
+    </div>
+
+  );
+}
 }
 
 export default App;
--- src/Clicky/Clicky.js
+++ src/Clicky/Clicky.js
@@ -1,35 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import './Clicky.css'
 
-class Clicky extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { isClicked: false };
+ const Clicky =(props)=> (
 
-    }
-
-    clickHandler = () => {
-        if (this.state.isClicked === false) {
-
-            this.setState({ isClicked: true })
-            console.log("counter is " + this.state.isClicked)
-
-        }
-        else { //if button was already clicked I want the app is alert user and reset current score and keep high score
-
-            console.log("counter is " + this.state.isClicked)
-        }
-
-    }
-    render() {
-        return (
-            <div onClick={this.clickHandler} className='clicky'>
-                <img className='clickyimg' src={this.props.image} alt='img should be here' />
+            //everything you want in the clicky is a value to add to array and on click, a function to handle the logic in app.js
+            //classname is only for css purposes
+            <div value={props.id} onClick={()=>props.click(props.id)} className='clicky'>
+                <img className='clickyimg' src={props.image} alt='img should be here' />
             </div>
-        )
-    }
-
-}
-
+ )
+       
+    
 export default Clicky
